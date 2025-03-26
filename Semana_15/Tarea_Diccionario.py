c7edd9c011792187ae68e8f83d2a19240ab9ba5f
Corte-Nixon <cortenixon2007@gmail.com>
2025-03-26 00:02:45 -0500

    Tarea de la semana 15

--- Semana_15/Tarea_Diccionario.py
+++ Semana_15/Tarea_Diccionario.py
@@ -1,9 +1,9 @@
 # Crear un diccionario con información personal
 información_personal = {
-    "nombre" : "Dario",
-    "edad" : 25,
-    "ciudad" : "Nueva Loja",
-    "profesión" : "Abogado"
+    "nombre": "Dario",
+    "edad": 25,
+    "ciudad": "Nueva Loja",
+    "profesión": "Abogado"
 }
 
 # Acceder y modificar la ciudad
@@ -13,11 +13,11 @@ información_personal["ciudad"] = "Quito"
 información_personal["profesión"] = "Master penalista"
 
 # Verificar existencia de la clave "telefono"
-if "telefono" not in información_personal :
-    información_personal["telefono"] : "394-546-4342"
+if "telefono" not in información_personal:
+    información_personal["telefono"] = "394-546-4342"  # Corrección aquí
 
 # Eliminar la clave "edad"
-if "edad" in información_personal :
+if "edad" in información_personal:
     del información_personal["edad"]
 
 # Imprimir el diccionario Final
